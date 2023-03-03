e3ac1f14c171819a94dd95ff848c4f752984263b
Mansoor Akhter <mansoor21st@Gmail.com>
2023-03-03 15:44:02 +0500

    next js 13 & chakra ui landing page

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import Features from "@/components/Features"
+import Footer from "@/components/Footer"
+import HeroSection from "@/components/HeroSection"
+import Testimonials from "@/components/Testimonials"
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Features />
+      <Testimonials />
+      <Footer />
+    </>
+  )
+}
